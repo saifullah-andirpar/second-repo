7a1ca90bc0addfc766b0ba5ef452aa39bf68caab
saifullah-andirpar <saifullah_andirpar@yahoo.com>
2025-02-08 07:50:47 +0600

    add

---
+++ bangladesh.js
@@ -0,0 +1,103 @@
+// import info from './Export.js'
+// console.log (info.product,info.user,info.add (5,10))
+
+
+// import {product} from './Export.js'
+// console.log (product)
+
+
+// Lession-01
+// import {product, user} from './Export.js'
+// console.log (product, user)
+
+
+
+// Lession-02
+// import {product, user} from './Export.js'
+// console.log (product, user)
+
+
+
+// Lession-03
+// import {product, user as u} from './Export.js'
+// console.log (product, u)
+
+
+
+
+// Lession-04
+// import {product, user as u,add} from './Export.js'
+// console.log (product, u)
+// add (5,10)
+
+
+
+// Lession-05
+// import add from './Export.js'
+// add (5,10)
+
+
+
+// Lession-06
+import {product, user} from './Export.js'
+console.log (product,user)
+import add from './Export.js'
+add (5,10)
+
+// Lession-06A
+// import {product,user } from "./Export.js";
+// console.log (product,user)
+// import add from './Export.js'
+// const sum = add (4,9)
+// console.log (sum)
+
+
+
+
+// import { product,user } from "./Export.js";
+// console.log (product,user)
+// import add from './Export.js'
+// add (3,9)
+// const sum = add (4,8)
+// console.log (add (6,9))
+
+// import {product, user} from "./Export.js";
+// console.log (product,user)
+// import add from './Export.js';
+// add (3,9)
+// const sum = add (4,8)
+// console.log (add (8,7))
+
+// Lession-07
+// import add, {product, user} from './Export.js'
+// console.log (product,user)
+// add (5,10)
+
+// import {product, user} from "./Export.js";
+// console.log (product,user)
+// import add from './Export.js';
+// add (3,9)
+// const sum = add (4,8)
+// console.log (add (8,7))
+
+
+
+// Lession-08
+// import info from './Export.js'
+// console.log (info.product,info.user,info.add (5,10))
+
+
+
+// Lession-09
+// import info from './Export.js'
+// console.log (info.product, info.user)
+// info.add (3,8)
+
+
+
+
+
+
+
+
+
